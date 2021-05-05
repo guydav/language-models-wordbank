7999b9d7f72d86e0f511b6899cea0c445b15c9a7
guydav <gd1279@nyu.edu>
2021-05-05 12:12:36 -0400

    Adding outputs for discriminative task, a bunch of models

--- model/run_multiple_models_discrimintative.bash
+++ model/run_multiple_models_discrimintative.bash
@@ -1,8 +1,9 @@
 #!/usr/local/bin/bash
 
-RemainingModels=("nyu-mll/roberta-base-100M-1" "nyu-mll/roberta-base-100M-2" "nyu-mll/roberta-base-100M-3" "nyu-mll/roberta-base-1B-1" "nyu-mll/roberta-base-1B-2" "nyu-mll/roberta-base-1B-3" "bert-base-uncased" "distilbert-base-uncased")
+RemainingModels=("nyu-mll/roberta-med-small-1M-1" "nyu-mll/roberta-med-small-1M-2" "nyu-mll/roberta-med-small-1M-3" "nyu-mll/roberta-base-10M-1" "nyu-mll/roberta-base-10M-2" "nyu-mll/roberta-base-10M-3"  "nyu-mll/roberta-base-100M-1" "nyu-mll/roberta-base-100M-2" "nyu-mll/roberta-base-100M-3" "nyu-mll/roberta-base-1B-1" "nyu-mll/roberta-base-1B-2" "nyu-mll/roberta-base-1B-3" "bert-base-uncased" "distilbert-base-uncased")
 
 for modelName in ${RemainingModels[*]}; do
-    python run_discriminative_task.py -c "${modelName}"
+    echo python run_discriminative_task.py -c "${modelName}" -o "../output/discriminative/both-datasets-10-10"
+    python run_discriminative_task.py -c "${modelName}" -o "../output/discriminative/both-datasets-10-10"
 done
 
