70c1a061820a69bdb0dc363520ad995ee6c71815
guydav <gd1279@nyu.edu>
2021-05-08 19:50:38 -0400

    Running script

--- model/run_multiple_models_discrimintative.bash
+++ model/run_multiple_models_discrimintative.bash
@@ -3,7 +3,7 @@
 RemainingModels=("nyu-mll/roberta-med-small-1M-1" "nyu-mll/roberta-med-small-1M-2" "nyu-mll/roberta-med-small-1M-3" "nyu-mll/roberta-base-10M-1" "nyu-mll/roberta-base-10M-2" "nyu-mll/roberta-base-10M-3"  "nyu-mll/roberta-base-100M-1" "nyu-mll/roberta-base-100M-2" "nyu-mll/roberta-base-100M-3" "nyu-mll/roberta-base-1B-1" "nyu-mll/roberta-base-1B-2" "nyu-mll/roberta-base-1B-3" "bert-base-uncased" "distilbert-base-uncased")
 
 for modelName in ${RemainingModels[*]}; do
-    echo python run_discriminative_task.py -c "${modelName}" -o "../output/discriminative/both-datasets-10-10"
-    python run_discriminative_task.py -c "${modelName}" -o "../output/discriminative/both-datasets-10-10"
+    echo python run_discriminative_task.py -c "${modelName}" -o "../output/discriminative/both-datasets-100-all"
+    python run_discriminative_task.py -s 100 -c "${modelName}" -o "../output/discriminative/both-datasets-100-all"
 done
 
