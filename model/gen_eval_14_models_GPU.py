removing the index bug that gave wrong results when we ran with both datasets

model/gen_eval_14_models_GPU.py
@@ -8,7 +8,7 @@ from math import ceil
 
 DEBUG = 1
 #Do not repeat this RUN_ID. Every time you run, use a new one. Also, use different ranges so that we don't overlap.
-RUN_ID = "30_GPU_14_models_babi_all_words"
+RUN_ID = "28_GPU_14_models_Childes_babi_all_words"
 
 #Read words from wordbank
 def read_wordbank(wordbank):
@@ -30,9 +30,10 @@ print("\nWordbank: ", wordbank)
 #'high chair', 'him', 'lawn mower', 'leg', 'let me', 'so big!', 'soap',
 #'this', 'this little piggy', 'yourself', 'yucky', 'yum yum', 'zebra', 'zipper', 'zoo']
 
+
 #Read childes sentences along with the matches from wordbank
-def read_dataset(dataset_sentences, word_occurences_in_dataset, dataset_file_path):
-    i = 0
+def read_dataset(dataset_sentences, word_occurences_in_dataset, dataset_file_path, no_of_sentences_read_total):
+    i = no_of_sentences_read_total
     with open(dataset_file_path, 'r') as dataset_file:
         #Store in hashmaps? Or any other better methods? The size is small
         #and maynot require a database.
@@ -94,12 +95,13 @@ def generate_predictions_multiple_models(checkpoint_name_list, max_words_to_eval
         sys.exit()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     
+    no_of_sentences_read_total = 0
     if(datasets in ["childes", "Childes", "both"]):
-        no_lines_read = read_dataset(dataset_sentences, word_occurences_in_dataset, childes_file_path)
-        print("\nFinished reading Childes data from " + str(no_lines_read) + " lines.")
+        no_of_sentences_read_total = read_dataset(dataset_sentences, word_occurences_in_dataset, childes_file_path, no_of_sentences_read_total)
+        print("\nTotal sentences read so far  " + str(no_of_sentences_read_total) + " lines.")
     if(datasets in ["babi", "bAbi", "both"]):
-        no_lines_read = read_dataset(dataset_sentences, word_occurences_in_dataset, bAbi_file_path)
-        print("\nFinished reading bAbi data from " + str(no_lines_read) + " lines.")
+        no_of_sentences_read_total = read_dataset(dataset_sentences, word_occurences_in_dataset, bAbi_file_path, no_of_sentences_read_total)
+        print("\nTotal sentences read so far " + str(no_of_sentences_read_total) + " lines.")
     if(datasets not in ["childes", "Childes", "babi", "bAbi", "both"]):
         print("Options for datasets argument: childes, Childes, babi, bAbi, both")
     
@@ -329,7 +331,7 @@ checkpoint_names = [checkpoint_name_4, checkpoint_name_4_2, checkpoint_name_4_3,
                     checkpoint_name_5, \
                     checkpoint_name_6]
 
-datasets_to_be_read = "babi"#options: "childes" or "Childes", "bAbi" or "babi", "both"
+datasets_to_be_read = "both"#options: "childes" or "Childes", "bAbi" or "babi", "both"
 generate_predictions_multiple_models(checkpoint_names, max_words_to_evaluate = 11000, 
                                      scoring="top_k", k = 20, no_of_sentences_per_word = 10,
                                      min_prob = 0.1, cutoff = 0.5, datasets = datasets_to_be_read)

model/gen_eval_multiple_models_liberally_extended_GPU.py
@@ -31,8 +31,8 @@ print("\nWordbank: ", wordbank)
 #'this', 'this little piggy', 'yourself', 'yucky', 'yum yum', 'zebra', 'zipper', 'zoo']
 
 #Read childes sentences along with the matches from wordbank
-def read_dataset(dataset_sentences, word_occurences_in_dataset, dataset_file_path):
-    i = 0
+def read_dataset(dataset_sentences, word_occurences_in_dataset, dataset_file_path, no_of_sentences_read_total):
+    i = no_of_sentences_read_total
     with open(dataset_file_path, 'r') as dataset_file:
         #Store in hashmaps? Or any other better methods? The size is small
         #and maynot require a database.
@@ -94,12 +94,13 @@ def generate_predictions_multiple_models(checkpoint_name_list, max_words_to_eval
         sys.exit()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     
+    no_of_sentences_read_total = 0
     if(datasets in ["childes", "Childes", "both"]):
-        no_lines_read = read_dataset(dataset_sentences, word_occurences_in_dataset, childes_file_path)
-        print("\nFinished reading Childes data from " + str(no_lines_read) + " lines.")
+        no_of_sentences_read_total = read_dataset(dataset_sentences, word_occurences_in_dataset, childes_file_path, no_of_sentences_read_total)
+        print("\nTotal sentences read so far  " + str(no_of_sentences_read_total) + " lines.")
     if(datasets in ["babi", "bAbi", "both"]):
-        no_lines_read = read_dataset(dataset_sentences, word_occurences_in_dataset, bAbi_file_path)
-        print("\nFinished reading bAbi data from " + str(no_lines_read) + " lines.")
+        no_of_sentences_read_total = read_dataset(dataset_sentences, word_occurences_in_dataset, bAbi_file_path, no_of_sentences_read_total)
+        print("\nTotal sentences read so far " + str(no_of_sentences_read_total) + " lines.")
     if(datasets not in ["childes", "Childes", "babi", "bAbi", "both"]):
         print("Options for datasets argument: childes, Childes, babi, bAbi, both")
     