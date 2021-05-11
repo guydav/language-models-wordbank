48cdb680cafd6c34deb5a172aa9bbb7c0142b387
Guy Davidson <guydav@gmail.com>
2021-05-11 17:41:45 -0400

    Revised discriminative task output script. Added a default order to output the results in. Removed the row names for all zeros and ones (`IRT.R` adds them).

--- model/discriminative_task_output_to_mirt_input.py
+++ model/discriminative_task_output_to_mirt_input.py
@@ -20,6 +20,24 @@ parser.add_argument('-c', '--result-column', default=DEFAULT_RESULT_COL)
 parser.add_argument('-a', '--add-zero-one-rows', action='store_true', help='Add rows of ones and zeros?')
 
 
+DEFAULT_MODEL_ORDER = (
+    'nyu-mll/roberta-med-small-1M-1',
+    'nyu-mll/roberta-med-small-1M-2',
+    'nyu-mll/roberta-med-small-1M-3',
+    'nyu-mll/roberta-base-10M-1',
+    'nyu-mll/roberta-base-10M-2',
+    'nyu-mll/roberta-base-10M-3',
+    'nyu-mll/roberta-base-100M-1',
+    'nyu-mll/roberta-base-100M-2',
+    'nyu-mll/roberta-base-100M-3',
+    'nyu-mll/roberta-base-1B-1',
+    'nyu-mll/roberta-base-1B-2',
+    'nyu-mll/roberta-base-1B-3',
+    'bert-base-uncased',
+    'distilbert-base-uncased',
+)
+
+
 def top_k(df, args):
     scores = [np.fromstring(x[1:-1], sep=',') for x in df.sentence_scores]
     df['new_results'] = [0 in np.argpartition(a, -args.k)[-args.k:] for a in scores]
@@ -32,6 +50,22 @@ def aggregate_by_word_and_sentence(df, args):
 
 def model_name_from_file_name(name):
     return name[:name.find('_sentences')].replace('_', '/')
+
+
+def scores_from_model_paths(paths, args):
+    df = None
+    for input_file in paths:
+        if df is None:
+            df = pd.read_csv(input_file)
+        else:
+            df = df.append(pd.read_csv(input_file))
+    
+    if args.rescore_function is not None:
+        word_scores = globals()[args.rescore_function](df, args)
+    else:
+        word_scores = aggregate_by_word_and_sentence(df, args)
+
+    return list(word_scores), list(df.word.unique())
 
 
 def main(args):
@@ -47,33 +81,35 @@ def main(args):
     for name, path in input_file_names_and_paths:
         models_to_paths[model_name_from_file_name(name)].append(path)
 
-    for model_name in tqdm.tqdm(models_to_paths):
-        models.append(model_name)
-        df = None
-        for input_file in models_to_paths[model_name]:
-            if df is None:
-                df = pd.read_csv(input_file)
-            else:
-                df = df.append(pd.read_csv(input_file))
-        
-        if args.rescore_function is not None:
-            word_scores = globals()[args.rescore_function](df, args)
-        else:
-            word_scores = aggregate_by_word_and_sentence(df, args)
+    all_models = list(models_to_paths.keys())
 
-        results.append(list(word_scores))
+    for model_name in tqdm.tqdm(DEFAULT_MODEL_ORDER):
+        if model_name in all_models:
+            all_models.remove(model_name)
+
+            models.append(model_name)
+            model_results, model_words = scores_from_model_paths(models_to_paths[model_name], args)
+            results.append(model_results)
+            if words is None:
+                words = model_words
+
+    for model_name in all_models:
+        models.append(model_name)
+        model_results, model_words = scores_from_model_paths(models_to_paths[model_name], args)
+        results.append(model_results)
         if words is None:
-            words = list(df.word.unique())
+            words = model_words
 
     if args.add_zero_one_rows:
         ones = [1] * len(results[-1])
         results.append(ones)
-        models.append('all-ones')
+        # IRT.R script adds these
+        # models.append('all-ones')
 
         zeros = [0] * len(results[-1])
         results.append(zeros)
-        models.append('all-zeros')
-        
+        # IRT.R script adds these
+        # models.append('all-zeros')
 
     output_df = pd.DataFrame(results, columns=words)
     output_df.to_csv(input_folder_path / 'irt_data.tsv', sep='\t', index=False)
